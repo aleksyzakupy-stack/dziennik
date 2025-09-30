e6dd0390e5263150494eb7d65aae83dd90b61b1b
aleksyzakupy-stack <aleksyzakupy@gmail.com>
2025-09-30 22:48:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ with st.sidebar.form("register_form"):
             st.sidebar.success("✅ Rejestracja udana! Możesz się zalogować.")
 
 # --- Logowanie ---
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login(form_name="Login", location="sidebar")
 
 if authentication_status == False:
     st.error("❌ Nieprawidłowy login lub hasło")
